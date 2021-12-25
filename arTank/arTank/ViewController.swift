ddce06fa145d70f5f82f40a931ef531989fbd82c
syhien <syhien@outlook.at>
2021-12-25 20:23:52 +0800

    画好ui

--- arTank/arTank/ViewController.swift
+++ arTank/arTank/ViewController.swift
@@ -11,14 +11,13 @@ import RealityKit
 class ViewController: UIViewController {
     
     @IBOutlet var arView: ARView!
+    var tankAnchor: TinyToyTank._TinyToyTank?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Load the "Box" scene from the "Experience" Reality File
-        let boxAnchor = try! Experience.loadBox()
-        
+        tankAnchor = try! TinyToyTank.load_TinyToyTank()
         // Add the box anchor to the scene
-        arView.scene.anchors.append(boxAnchor)
+        arView.scene.anchors.append(tankAnchor!)
     }
 }
